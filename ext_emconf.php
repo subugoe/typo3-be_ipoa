cb4e8450001408f3ce74df5090b915159083173c
snaegle <snaegle@users.noreply.github.com>
2018-07-11 15:03:47 +0200

    Update ext_emconf.php
    Allow higher Typo3 versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
     'constraints' =>
     array (
         'depends' => array (
-            'typo3' => '6.2.0-6.2.999',
+            'typo3' => '6.2.0-8.7.99',
         ),
         'conflicts' => '',
     ),
